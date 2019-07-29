9916df96e1fc10bde17b4bbe81cc63abf658a28c
skatecram <cram.skate@hotmail.com>
2019-07-29 10:35:11 +0000

    fix delete Bugreport

--- resources/views/admin/bugreports/index.blade.php
+++ resources/views/admin/bugreports/index.blade.php
@@ -119,7 +119,7 @@
                                     </a>
                                 @endcan
                                 @can('bugreport_delete')
-                                    <form action="{{ action('Admin\BugreportsCommentController@destroy', [ 'id' => $bugreport->id]) }}" method="POST" onsubmit="return confirm('{{ trans('global.areYouSure') }}');" style="display: inline-block;">
+                                    <form action="{{ action('Admin\Bugreports@destroy', [ 'id' => $bugreport->id]) }}" method="POST" onsubmit="return confirm('{{ trans('global.areYouSure') }}');" style="display: inline-block;">
                                         <input type="hidden" name="_method" value="DELETE">
                                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
                                         <input type="submit" class="btn btn-xs btn-danger" value="{{ trans('global.delete') }}">
