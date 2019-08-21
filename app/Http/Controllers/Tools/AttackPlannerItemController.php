f95dd49043af395195992d120a48e03b48584841
skatecram <cram.skate@hotmail.com>
2019-08-21 20:46:47 +0000

    commit for MKich

--- app/Http/Controllers/Tools/AttackPlannerItemController.php
+++ app/Http/Controllers/Tools/AttackPlannerItemController.php
@@ -13,6 +13,7 @@ use App\Tool\AttackPlanner\AttackList;
 use App\Tool\AttackPlanner\AttackListItem;
 use App\User;
 use App\Util\BasicFunctions;
+use App\Util\Icon;
 use App\World;
 use http\Url;
 use App\Http\Requests\StoreAttackPlannerItemRequest;
@@ -23,6 +24,7 @@ use Yajra\DataTables\Facades\DataTables;
 
 class AttackPlannerItemController extends BaseController
 {
+    private $dbName;
 
     public function store(StoreAttackPlannerItemRequest $request)
     {
@@ -40,18 +42,62 @@ class AttackPlannerItemController extends BaseController
 
     public function data(AttackList $attackList){
         $items = $attackList->items;
+        $world = $attackList->world;
+        $this->dbName = BasicFunctions::getDatabaseName($world->server->code, $world->name);
 
         return Datatables::collection($items)
+            ->editColumn('start_village_id', function (AttackListItem $attackListItem) {
+                $village =$attackListItem->start_village;
+                return '['.$village->x.'|'.$village->y.'] '.BasicFunctions::outputName($village->name);
+            })
+            ->editColumn('target_village_id', function (AttackListItem $attackListItem) {
+                $village =$attackListItem->target_village;
+                return '['.$village->x.'|'.$village->y.'] '.BasicFunctions::outputName($village->name);
+            })
+            ->editColumn('type', function (AttackListItem $attackListItem) {
+                return self::type_img($attackListItem->type);
+            })
+            ->editColumn('slowest_unit', function (AttackListItem $attackListItem) {
+                return '<img id="type_img" src="'.Icon::icons($attackListItem->slowest_unit).'">';
+            })
             ->addColumn('attacker', function (AttackListItem $attackListItem) {
-                return 'attacker';
+                return BasicFunctions::outputName($attackListItem->start_village->playerLatest->name);
             })
             ->addColumn('defender', function (AttackListItem $attackListItem) {
-                return 'defender';
+                return BasicFunctions::outputName($attackListItem->target_village->playerLatest->name);
+            })
+            ->addColumn('send_time', function (AttackListItem $attackListItem) {
+                return $attackListItem->send_time->format('d.m.y H:i:s');
+            })
+            ->addColumn('arrival_time', function (AttackListItem $attackListItem) {
+                return $attackListItem->arrival_time->format('d.m.y H:i:s');
             })
             ->addColumn('time', function (AttackListItem $attackListItem) {
                 return $attackListItem->send_time;
             })
+            ->rawColumns(['type', 'slowest_unit'])
             ->toJson();
+    }
+
+    public static function type_img($type){
+        switch ($type){
+            case 0:
+                return '<img id="type_img" src="'.asset('images/ds_images/unit/unit_ram.png').'">';
+            case 1:
+                return '<img id="type_img" src="'.asset('images/ds_images/unit/unit_snob.png').'">';
+            case 2:
+                return '<img id="type_img" src="'.asset('images/ds_images/unit/fake.png').'">';
+            case 3:
+                return '<img id="type_img" src="'.asset('images/ds_images/unit/wall.png').'">';
+            case 4:
+                return '<img id="type_img" src="'.asset('images/ds_images/unit/unit_spear.png').'">';
+            case 5:
+                return '<img id="type_img" src="'.asset('images/ds_images/unit/unit_sword.png').'">';
+            case 6:
+                return '<img id="type_img" src="'.asset('images/ds_images/unit/unit_heavy.png').'">';
+            case 7:
+                return '<img id="type_img" src="'.asset('images/ds_images/unit/def_fake.png').'">';
+        }
     }
 
 }
