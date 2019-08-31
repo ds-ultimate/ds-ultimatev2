1d1101d57e6a42685182ef1a0a50eec11fbc8312
extremeCrazyCoder <MKich@gmx.at>
2019-08-31 20:35:13 +0000

    Testfunktionen entfernt

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,6 @@ Auth::routes(['verify' => true]);
 Route::get('/home', 'HomeController@index')->name('home')->middleware('verified');
 
 Route::get('/test', function () {
-    return view('test');
 });
 
 Route::get('/setlocale/{locale}',function($lang){
