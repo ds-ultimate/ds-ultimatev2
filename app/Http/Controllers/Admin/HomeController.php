b1ccbe0930174eeee41f6509ecee7daaa60de1d4
skatecram <cram.skate@hotmail.com>
2019-09-18 19:26:51 +0200

    add Dashboard Counters

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -2,10 +2,19 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Tool\AttackPlanner\AttackList;
+use App\Tool\AttackPlanner\AttackListItem;
+use App\Tool\Map\Map;
+
 class HomeController
 {
     public function index()
     {
-        return view('admin.home');
+
+        $counter['maps'] = Map::all()->count();
+        $counter['attackplaner'] = AttackList::all()->count();
+        $counter['attacks'] = AttackListItem::all()->count();
+
+        return view('admin.home', compact('counter'));
     }
 }
--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -9,6 +9,44 @@
             </div>
         </div>
     </div>
+    <div class="row">
+        <!-- Column -->
+        <div class="col-md-6 col-lg-3 col-xlg-3">
+            <div class="card card-inverse bg-info">
+                <div class="box bg-info text-center p-3">
+                    <h1 class="font-light text-white">{{ \App\Bugreport::countNew() }}</h1>
+                    <h6 class="text-white">{{ __('cruds.bugreport.new') }} {{ __('cruds.bugreport.title') }}</h6>
+                </div>
+            </div>
+        </div>
+        <!-- Column -->
+        <div class="col-md-6 col-lg-3 col-xlg-3">
+            <div class="card card-inverse bg-warning">
+                <div class="box text-center p-3">
+                    <h1 class="font-light text-white">{{ \App\Util\BasicFunctions::numberConv($counter['maps']) }}</h1>
+                    <h6 class="text-white">{{ __('ui.tool.map.title') }}</h6>
+                </div>
+            </div>
+        </div>
+        <!-- Column -->
+        <div class="col-md-6 col-lg-3 col-xlg-3">
+            <div class="card card-primary bg-danger">
+                <div class="box text-center p-3">
+                    <h1 class="font-light text-white">{{ \App\Util\BasicFunctions::numberConv($counter['attackplaner']) }}</h1>
+                    <h6 class="text-white">{{ __('ui.tool.attackPlanner.title') }}</h6>
+                </div>
+            </div>
+        </div>
+        <!-- Column -->
+        <div class="col-md-6 col-lg-3 col-xlg-3">
+            <div class="card card-inverse bg-success">
+                <div class="box text-center p-3">
+                    <h1 class="font-light text-white">{{ \App\Util\BasicFunctions::numberConv($counter['attacks']) }}</h1>
+                    <h6 class="text-white">{{ __('ui.tool.attackPlanner.attackTotal') }}</h6>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
 @section('scripts')
