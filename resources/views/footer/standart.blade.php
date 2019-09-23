888888cf0b4ac719038fff044eb42727988f0cc5
skatecram <cram.skate@hotmail.com>
2019-09-23 23:33:20 +0200

    footer update with team

--- resources/views/footer/standart.blade.php
+++ resources/views/footer/standart.blade.php
@@ -1,24 +1,30 @@
 <nav class="navbar fixed-bottom navbar-light bg-footer">
-    <div class="col-12 mx-auto text-center">
+    <div class="col-12 px-0 text-center">
         <ul class="list-inline my-0">
-            <li class="list-inline-item">
+            <li class="list-inline-item mr-0">
                 <small><a href="{{ route('legalPage') }}">Impressum</a></small>
             </li>
-            <li class="list-inline-item">
+            <li class="list-inline-item mr-0">
                 <small>-</small>
             </li>
-            <li class="list-inline-item">
+            <li class="list-inline-item mr-0">
                 <small><a href="{{ route('changelog') }}">Changelog</a></small>
             </li>
-            <li class="list-inline-item">
-                <small> -</small>
+            <li class="list-inline-item mr-0">
+                <small>-</small>
             </li>
-            <li class="list-inline-item">
+            <li class="list-inline-item mr-0">
+                <small><a href="{{ route('team') }}">Team</a></small>
+            </li>
+            <li class="list-inline-item mr-0">
+                <small>-</small>
+            </li>
+            <li class="list-inline-item mr-0">
                 <small><a href="{{ route('form.bugreport') }}">{{ __('user.bugreport.title') }}</a></small>
             </li>
             <li class="list-inline-item">
                 <div class="dropup">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <a class="nav-link dropdown-toggle p-2" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <small>Discord</small>
                     </a>
                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown" style="background-color: transparent; border: transparent; border: none;">
