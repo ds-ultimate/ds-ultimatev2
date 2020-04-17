687c81622b439a4f7eea75421da241b6dc47626f
extremeCrazyCoder <MKich@gmx.at>
2020-04-17 15:27:33 +0200

    Hotfix for map routing

--- app/Http/Controllers/API/FindModelController.php
+++ app/Http/Controllers/API/FindModelController.php
@@ -13,31 +13,31 @@ use App\Http\Resources\Village as VillageResource;
 
 class FindModelController extends Controller
 {
-    public static function getVillageByCoord($server, $world, $x, $y){
+    public function getVillageByCoord($server, $world, $x, $y){
         $villageModel = new Village();
         $villageModel->setTable(BasicFunctions::getDatabaseName($server, $world).'.village_latest');
 
         return new VillageResource($villageModel->where(['x' => $x, 'y' => $y])->first());
     }
 
-    public static function getPlayerByName($server, $world, $name){
+    public function getPlayerByName($server, $world, $name){
         $playerModel = new Player();
         $playerModel->setTable(BasicFunctions::getDatabaseName($server, $world).'.player_latest');
 
         return new PlayerResource($playerModel->where('name', urlencode($name))->first());
     }
 
-    public static function getAllyByName($server, $world, $name){
+    public function getAllyByName($server, $world, $name){
         $allyModel = new Ally();
         $allyModel->setTable(BasicFunctions::getDatabaseName($server, $world).'.ally_latest');
 
         return new AllyResource($allyModel->where('name', urlencode($name))->orWhere('tag', urlencode($name))->first());
     }
 
-    public static function getSelect2Player($server, $world){
+    public function getSelect2Player($server, $world){
         $playerModel = new Player();
         $playerModel->setTable(BasicFunctions::getDatabaseName($server, $world).'.player_latest');
-        return APIController::select2return($playerModel, array('name'), function($rawData) {
+        return $this->select2return($playerModel, array('name'), function($rawData) {
             return array(
                 'id' => $rawData->playerID,
                 'text' => BasicFunctions::decodeName($rawData->name),
@@ -45,10 +45,10 @@ class FindModelController extends Controller
         });
     }
 
-    public static function getSelect2Ally($server, $world){
+    public function getSelect2Ally($server, $world){
         $allyModel = new Ally();
         $allyModel->setTable(BasicFunctions::getDatabaseName($server, $world).'.ally_latest');
-        return APIController::select2return($allyModel, array('name', 'tag'), function($rawData) {
+        return $this->select2return($allyModel, array('name', 'tag'), function($rawData) {
             return array(
                 'id' => $rawData->allyID,
                 'text' => BasicFunctions::decodeName($rawData->name) . ' [' . BasicFunctions::decodeName($rawData->tag) . ']',
@@ -56,7 +56,7 @@ class FindModelController extends Controller
         });
     }
     
-    private static function select2return(\Illuminate\Database\Eloquent\Model $model, $searchIn, callable $extractOne) {
+    private function select2return(\Illuminate\Database\Eloquent\Model $model, $searchIn, callable $extractOne) {
         $getArray = \Illuminate\Support\Facades\Request::input();
         $perPage = 50;
         $search = (isset($getArray['search']))?('%'.BasicFunctions::likeSaveEscape(urlencode($getArray['search'])).'%'):('%');
