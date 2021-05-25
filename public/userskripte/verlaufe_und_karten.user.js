c2c883a85093438cf36defc1b96b09f9a690f7da
extremeCrazyCoder <MKich@gmx.at>
2021-05-25 10:26:56 +0200

    use official version of the userskript

--- public/userskripte/verlaufe_und_karten.user.js
+++ public/userskripte/verlaufe_und_karten.user.js
@@ -2,7 +2,7 @@
 // @name           DS-Ultimate Verlaeufe
 // @description    Die Staemme: Fuegt bei der Info-Seite von Spieler und Staemme Bash- und Punkteuebersichten von DS-Ultimate ein.
 // @namespace      https://ds-ultimate.de
-// @version        1.4
+// @version        1.5
 // @downloadURL    https://ds-ultimate.de/userskripte/verlaufe_und_karten.user.js
 // @updateURL      https://ds-ultimate.de/userskripte/verlaufe_und_karten.user.js
 // @include        https://de*.die-staemme.de/game.php*screen=info_player*
@@ -13,60 +13,16 @@
 // @grant none
 // ==/UserScript==
 
-win = typeof unsafeWindow != 'undefined' ? unsafeWindow : window;
+/*
+Copyright 2021 DS-Ultimate Team (@author)
+ 
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ 
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ 
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
 
-var getUrlParameter = function getUrlParameter(sParam) {
-    var sPageURL = win.location.search.substring(1),
-        sURLVariables = sPageURL.split('&'),
-        sParameterName,
-        i;
-
-    for (i = 0; i < sURLVariables.length; i++) {
-        sParameterName = sURLVariables[i].split('=');
-
-        if (sParameterName[0] === sParam) {
-            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
-        }
-    }
-};
-
-var generateDSUltimateImage = function(world, type, id, diagramType, width) {
-  var data = '<a id="" href="https://www.ds-ultimate.de/' + world[0] + '/' + world[1] + '/' + type[1] + '/' + id + '" ';
-  data += 'target="_blank">';
-  data += '<img src="https://www.ds-ultimate.de/api/picture/' + world[0] + '-' + world[1] + '-' + type[0] + '-' + id + '-' +
-      diagramType + '-' + width + '-' + Math.round(width/3) + '.png">';
-  data += '</a>';
-  return data;
-}
-
-var host = win.location.host;
-var worldName = host.split('.');
-var world = [worldName[0].substring(0,2), worldName[0].substring(2)];
-var id = (getUrlParameter('id'))?getUrlParameter('id'): win.game_data.player.id;
-
-var element, width, type;
-if(getUrlParameter('screen') == 'info_player'){
-  element = $('#player_info');
-  width = element.width();
-  type = ['p', 'player'];
-  bash = true;
-}
-if(getUrlParameter('screen') == 'info_ally'){
-  element = $($('.vis')[0]);
-  width = element.width();
-  type = ['a', 'ally'];
-  bash = true;
-}
-if(getUrlParameter('screen') == 'info_village'){
-  element = $('td#embedmap_village').parent().parent().parent();
-  width = element.width();
-  type = ['v', 'village'];
-  bash = false;
-}
-
-if(element) {
-  $('tbody', element).append('<tr><td colspan="2">' + generateDSUltimateImage(world, type, id, 'points', width) + '</td></tr>');
-  if(bash){
-    $('tbody', element).append('<tr><td colspan="2">' + generateDSUltimateImage(world, type, id, 'gesBash', width) + '</td></tr>');
-  }
-}
+let win = typeof unsafeWindow != 'undefined' ? unsafeWindow : window;
+win.$.ajaxSetup({ cache: true });
+win.$.getScript('https://media.innogames.com/com_DS_DE/Scriptdatenbank/userscript_main/290_ds-ultimate_verlaeufe_mkich_skatecram.js');
