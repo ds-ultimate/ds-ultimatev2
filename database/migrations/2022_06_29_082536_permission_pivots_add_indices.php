ba7d1107524a2243e371de7d33154a7229e3d9f6
extremeCrazyCoder <MKich@gmx.at>
2022-06-29 08:29:30 +0200

    Add indices to the pemission pivot tables

---
+++ database/migrations/2022_06_29_082536_permission_pivots_add_indices.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('permission_role', function (Blueprint $table) {
+            $table->index('role_id');
+            $table->index('permission_id');
+        });
+        
+        Schema::table('role_user', function (Blueprint $table) {
+            $table->index('role_id');
+            $table->index('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
