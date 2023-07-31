757098f82849f9608a62a95243cb2488a0518935
extremeCrazyCoder <MKich@gmx.at>
2023-07-31 11:58:20 +0200

    Update hungarian translations provided by öreg

--- resources/lang/hu/tool.php
+++ resources/lang/hu/tool.php
@@ -4,14 +4,319 @@ return array (
   'distCalc' => 
   array (
     'title' => 'Távolság kalkulátor',
+    'days' => 'Nap(ok)',
+    'startVillage' => 'Indító falu',
+    'targetVillage' => 'Célfalu',
   ),
   'attackPlanner' => 
   array (
     'title' => 'Támadás tervező',
+    'arrivalTime' => 'Érkezési idő',
+    'attack' => 'Támadás',
+    'arrivetimeToSoon' => 'Az érkezési idő 1970 előtti',
+    'arrivetimeToLate' => 'Az érkezési idő túl messze van.',
+    'attackCount' => 'Nem választottál ki támadást szerkesztésre',
+    'attackCountTitle' => 'Támadás kiválasztása',
+    'attacker' => 'Támadó',
+    'attackStart_village' => 'Támadó falvak',
+    'attackTarget_village' => 'Célfalu',
+    'attackTotal' => 'Összes támadás',
+    'audioTiming' => 'A riasztás %S% mp-el az időzítő lejárta előtt indul.',
+    'confirm' => 
+    array (
+      'clear' => 'Minden támadás törölve lesz.',
+      'massDelete' => 'A kiválasztott támadások törölve lesznek',
+    ),
+    'conquest' => 'Foglalás',
+    'countdown' => 'Hátralévő idő',
+    'date' => 'Dátum',
+    'date_helper' => 'A támadás érkezésének napja',
+    'defender' => 'Védő',
+    'defensive' => 'Védekező',
+    'deleteAll' => 'Összes támadás törlése',
+    'deleteOutdated' => 'Lejárt támadások törlése',
+    'deleteSent' => 'Törölje a már elküldött támadásokat',
+    'destroyError' => 'A támadási tervet nem lehetett törölni. Ha úgy gondolod, hogy ez hiba, fordulj a csapathoz (a Discord segítségével vagy a probléma jelentésével a webhely alján).',
+    'destroySuccess' => 'A támadási terv sikeresen törölve lett',
+    'editLink' => 'Link szerkesztése',
+    'editLink_helper' => 'A link birtokában mindenki szerkesztheti a támadási tervet. Légy óvatos!',
+    'errorKoord' => 'Az indító - és a célfalu nem lehet ugyanaz',
+    'errorKoordEmpty' => 'Az indító - és a célfalu nem lehet üres',
+    'errorKoordTitle' => 'Falu koordináták',
+    'errorTitle' => 'Hiba',
+    'errorUnitCount' => 'Túl nagy a szám',
+    'exportBB' => 'Exportálás fórumra',
+    'exportBBDesc' => 'Exportálás fórumra',
+    'fake' => 'Fake',
+    'fakeSupport' => 'Fake erősítés',
+    'fastSupport' => 'Gyors erősítés',
+    'fastSwitch' => 'Saját támadási tervek',
+    'hints' => 
+    array (
+      'selectMultiple' => 
+      array (
+        'title' => 'Több támadás kiválasztása',
+        'desc' => 'Több támadás is kiválasztható a használatukkal: <div class="badge badge-primary h3">ctrl</div> [<div class="badge badge-primary">⌘</div>] és a <div class="badge badge-primary"><i class="fas fa-arrow-up"></i></div>',
+      ),
+      'workbenchImport' => 
+      array (
+        'title' => 'Támadások importálása a DS-Munkapadból',
+        'desc' => 'Nyisd meg a támadások áttekintését, és másold ki az összes importálandó támadást az alábbi billentyűkombinációval:<span class="truncate"><div class="badge badge-primary">ctrl</div> + <div class="badge badge-primary">c</div></span> <span class="truncate">[<div class="badge badge-primary">⌘</div> + <div class="badge badge-primary">c</div >]</span>. Ezt követően használja az import beviteli mezőt az Import/Export lapon belül.<span class="truncate"><div class="badge badge-primary">ctrl</div> + <div class="badge badge-primary">v< /div></span> <span class="truncate">[<div class="badge badge-primary">⌘</div> + <div class="badge badge-primary">v</div>] </span>.',
+      ),
+    ),
+    'icons' => 
+    array (
+      'ballBlue' => 'kék pont',
+      'ballGreen' => 'zöld pont',
+      'ballGrey' => 'szürke pont',
+      'ballRed' => 'piros pont',
+      'ballYellow' => 'sárga pont',
+      'devArcher' => 'Védelem íjászok ellen',
+      'devCav' => 'Védelem lovasság ellen',
+      'moveIn' => 'Zöld nyíl balra',
+      'moveOut' => 'Piros nyíl jobbra',
+      'wbAdd' => 'Hozzáadás',
+      'wbAlly' => 'Gyalogság elleni védekezés',
+      'wbCheckbox' => 'Kiválasztott jelölőnégyzet',
+      'wbDie' => 'Dobókocka',
+      'wbEye' => 'Szem',
+      'wbEyeForbidden' => 'Áthúzott szem',
+      'wbRemove' => 'Piros x',
+      'wbWarning' => 'Figyelem',
+    ),
+    'iconsTitle' => 'Ikonok',
+    'import' => 'Importálás',
+    'import_helper' => 'Importálj támadásokat a Munkapadodból',
+    'importExport' => 'Import/Export',
+    'importWBSuccess' => 'Sikeres importálás',
+    'importWBSuccessTitle' => 'A munkapad adatai sikeresen importálva lettek',
+    'links' => 'Linkek a megosztáshoz',
+    'multiedit' => 'Tömeges szerkesztés',
+    'export_helper' => 'Exportálás a Munkapadra',
+    'multieditSuccessTitle' => 'Tömeges szerkesztés',
+    'multieditSuccess' => 'Minden támadást sikeresen feldolgoztunk',
+    'offensive' => 'Támadó',
+    'otherUnits' => 'Egyéb egységek',
+    'sendTime' => 'Indítási idő',
+    'sendtimeToLate' => 'Az indítási idő túl messze van',
+    'sendtimeToSoon' => 'Az indítási idő 1970 előtti',
+    'show' => 'Mutasd a támadási tervet',
+    'showLink' => 'Link megtekintése',
+    'showLink_helper' => 'Ezzel a hivatkozással a támadási terv megtekinthető, de nem szerkeszthető.',
+    'standSupport' => 'Hosszú távú erősítés',
+    'startVillage' => 'Indító falu',
+    'startVillage_helper' => 'Az indító falu koordinátái',
+    'statistics' => 'Statisztika',
+    'storeError' => 'A támadás létrehozása nem működött',
+    'storeErrorTitle' => 'Hiba',
+    'storeSuccess' => 'A támadás sikeresen elkészült',
+    'storeSuccessTitle' => 'Sikeres',
+    'support' => 'Erősítés',
+    'targetVillage' => 'Célfalu',
+    'targetVillage_helper' => 'A célfalu koordinátái',
+    'time' => 'Idő',
+    'tips' => 'Tippek',
+    'time_helper' => 'A támadás érkezési ideje',
+    'type' => 'Típus',
+    'type_helper' => 'Támadás típusa',
+    'type_support_boost' => 'Erősítés növelése',
+    'type_tribe_boost' => 'Barátság(klán boost)',
+    'unit_helper' => 'Leglassabb egység',
+    'updateError' => 'A szerkesztést nem sikerült végrehajtani.',
+    'updateErrorTitle' => 'Hiba',
+    'updateSuccess' => 'A szerkesztés sikeres volt.',
+    'updateSuccessTitle' => 'Sikeres',
+    'uvMode' => 'Helyettes mód',
+    'uvModeDesc' => 'Az URL-ek a helyettesítésnek megfelelően lettek létrehozva',
+    'villageNotExist' => 'A falu nem létezik',
+    'villageNotExistStart' => 'Az indítófalu nem létezik',
+    'villageNotExistTarget' => 'A célfalu nem létezik',
+    'wallbreaker' => 'Faltörő',
+    'warnSending' => 'A küldés gomb használata saját felelősségre történik. Ha bármilyen problémába ütközöl, kérlek, értesítsd a DS-Ultimate csapatát (a Discordon vagy a Probléma bejelentése gombra kattintva)',
+    'wbImportWrongData' => 'Helytelen bemeneti adatok találhatók. Talán rossz formátum?',
+    'withoutTitle' => 'Ennek a támadás tervezőnek nincs címe, ezért alapértelmezés szerint mindig a „Támadástervező” gombra kattintva használhatod. Ahhoz, hogy új támadás tervezőt tudj létrehozni, adnod kell ennek egy címet.',
+    'export' => 
+    array (
+      'BB' => 
+      array (
+        'default' => 
+        array (
+          'body' => '[u][size=12][b]%TITLE%[/b][/size][/u]
+
+[b]Támadási terv[/b]
+Támadások száma: %ELEMENT_COUNT%
+[table]
+[**]ID[||]Típus[||]Egység[||]Indító falu[||]Célpont[||]Indítás ideje[||]Gyülekezőhely[/**]
+%ROW%
+[/table]
+
+[size=8]Létrehozva: %CREATE_AT%-kor a [url=%CREATE_WITHL%]%CREATE_WITH%[/url] használatával.[/size]',
+          'row' => '[*]%ELEMENT_ID%[|]%TYPE_IMG%[|]%UNIT%[|]%SOURCE%[|]%TARGET%[|]%SEND%[|]%PLACE%[/*]',
+        ),
+      ),
+      'IGM' => 
+      array (
+        'default' => 
+        array (
+          'body' => '[b]%TITLE%[/b]
+
+%ROW%
+
+Létrehozva: %CREATE_AT%-kor a [url=%CREATE_WITHL%]%CREATE_WITH%[/url] használatával.',
+          'row' => '%TYPE%  [b]%ATTACKER%[/b] játékostól  %SOURCE% faluból  %UNIT% egységgel  [b]%DEFENDER%[/b] játékos   %TARGET% falujára  [color=#ff0e0e]%SEND%[/color]-kor indul és [color=#2eb92e]%ARRIVE%[/color]-kor érkezik (%PLACE%)',
+        ),
+      ),
+    ),
+    'exportWBDesc' => 'Exportálás a DS Munkapadra.',
+    'exportWB' => 'DS Munkapad exportálása',
+    'exportIGM' => 'Exportálás játékon belüli üzenetben.',
+    'exportIGMDesc' => 'Exportálás játéküzenethez. Mivel a játékon belüli levelezések nem tudnak táblázatokat és képeket megjeleníteni, tiszta szöveg kerül megjelenítésre.',
   ),
   'map' => 
   array (
     'title' => 'Világ térkép',
+    'ally' => 'Szövetséges megjelölése',
+    'allySelectPlaceholder' => 'Szövetséges kiválasztása',
+    'autoUpdate' => 'Automatikus frissítések',
+    'autoZoom' => 'Automatikus zoom',
+    'autoUpdateHelp' => 'Ha aktiválva van, a világtérkép mindig az aktuális adatokat használja fel. Ha le van tiltva, a térkép egyszer jön létre, majd mindig a mentett képet használja.',
+    'cached' => 'Ez a térkép mentett képet használ, ezért az automatikus mentés le van tiltva. A beállítások módosítása esetén a térkép az aktuális adatokból jön létre. A cím a térkép megváltoztatása nélkül módosítható.',
+    'center' => 'A létrehozott térkép középpontja',
+    'copy' => 'Másolás',
+    'defaultBackground' => 'Háttérszín',
+    'defaultBarbarian' => 'A meg nem jelölt barbár falvak alapértelmezett színe',
+    'defaultPlayer' => 'A meg nem jelölt játékosfalvak alapértelmezett színe',
+    'deleteDrawing' => 'rajz törlése',
+    'destroyError' => 'A térképet nem sikerült törölni. Ha úgy gondolod, hogy ez hiba, fordulj a csapathoz (a Discord segítségével vagy a probléma jelentésével a webhely alján).',
+    'destroySuccess' => 'A térkép sikeresen törölve lett',
+    'drawer' => 
+    array (
+      'arrow' => 
+      array (
+        'drawSingle' => 'Rajzolj egy nyilat',
+        'drawTwo' => 'Rajzolj egy dupla nyilat',
+        'tooltip' => 'Vonalak és nyilak',
+      ),
+      'base' => 
+      array (
+        'tooltip' => 'Kattints a rajzoláshoz:',
+      ),
+      'border' => 
+      array (
+        'border' => 'Szegély',
+        'none' => 'Egyik sem',
+      ),
+      'brush' => 
+      array (
+        'size' => 'Méret',
+      ),
+      'canvas' => 
+      array (
+        'background' => 'Háttér',
+        'cancel' => 'Megszüntet',
+        'canvasProp' => 'Háttérbeállítások',
+        'center' => 'Központ',
+        'inline' => 'Sorban',
+        'left' => 'Balra',
+        'position' => 'Pozíció',
+        'right' => 'Jobbra',
+        'save' => 'Mentés',
+        'size' => 'Méret(pixel)',
+        'transparent' => 'Áttetsző',
+      ),
+      'circle' => 
+      array (
+        'tooltip' => 'Rajzolj egy kört',
+      ),
+      'color' => 
+      array (
+        'fill' => 'Kitöltés:',
+        'transparent' => 'Áttetsző',
+      ),
+      'fullscreen' => 
+      array (
+        'enter' => 'Teljes képernyős módba lépés',
+        'exit' => 'Kilépés a teljes képernyős módból',
+      ),
+      'general' => 
+      array (
+        'addDrawer' => 'Rajz hozzáadása',
+        'deleteCanvas' => 'Rajz törlése',
+        'deleteCanvasConfirm' => 'Biztosan törölni szeretnéd ezt a rajzot?',
+        'eraser' => 'Radír',
+        'freeDrawing' => 'Szabadkézi rajz',
+        'insert' => 'Beillesztés',
+        'insertDrawer' => 'Rajz beillesztése',
+        'simpleEraser' => 'Sima fehér radír',
+      ),
+      'line' => 
+      array (
+        'tooltip' => 'Rajzolj egy vonalat',
+      ),
+      'moveable' => 
+      array (
+        'moveCanvas' => 'Háttér mozgatása',
+      ),
+      'polygon' => 
+      array (
+        'newLine' => 'Kattints egy új sor kezdéséhez',
+        'stop' => 'A sokszög rajzolásának befejezése (esc)',
+        'tooltip' => 'Sokszög rajzolása',
+      ),
+      'rect' => 
+      array (
+        'tooltip' => 'Rajzolj egy téglalapot',
+      ),
+      'shape' => 
+      array (
+        'duplicate' => 'Megdupláz',
+        'remove' => 'Törlés',
+        'bringFront' => 'Teljesen előre hoz',
+        'bringForward' => 'Előrébb hoz',
+        'bringBack' => 'Teljesen hátra tesz',
+        'bringBackwards' => 'Hátrébb tesz',
+      ),
+      'triangle' => 
+      array (
+        'tooltip' => 'Háromszög rajzolása',
+      ),
+      'text' => 
+      array (
+        'tooltip' => 'Szöveg elhelyezése',
+        'newText' => 'Kattintson egy új szöveg elhelyezéséhez',
+        'font' => 'Betűtípus:',
+      ),
+    ),
+    'zoom' => 'Zoom',
+    'village' => 'Falu megjelölése',
+    'showText' => 'Jelölő hozzáadása',
+    'showPlayer' => 'Mutasd a játékos falvakat',
+    'showLink' => 'Link megtekintése',
+    'showLinkDesc' => 'Ezzel a linkkel a térkép megtekinthető, de nem szerkeszthető.',
+    'showContinentNumbers' => 'Mutasd a kontinensek számát',
+    'showBarbarian' => 'Mutasd a barbár falvakat',
+    'show' => 'Mutasd a térképet',
+    'showAllText' => 'Minden jelölés',
+    'settings' => 'Általános beállítások',
+    'resetDefault' => 'reset',
+    'playerSelectPlaceholder' => 'Játékos kiválasztása',
+    'player' => 'Játékos jelölése',
+    'markerFactor' => 'Falu távolság',
+    'links' => 'Linkek a térkép megosztásához',
+    'legend' => 'Legenda',
+    'highlightAll' => 'Emelje ki az összeset',
+    'highlight' => 'Emelje ki ezt',
+    'forumLinkDesc' => 'Használd a BB-kódokat, hogy megmutasd ezt a térképet a többi játékosnak a fórumokon',
+    'forumLink' => 'Fórum link',
+    'fastSwitch' => 'Saját térkép',
+    'editLinkDesc' => 'A link birtokában bárki szerkesztheti a térképet. Légy tehát óvatos, és csak azoknak add meg a linket, akikben megbízol!',
+    'editLink' => 'Link szerkesztése',
+    'edit' => 'Jelölők szerkesztése',
+    'drawingErr' => 'Hiba a rajzokban.
+Nyisd meg és zárd be a rajzfelületet.
+Ha a hiba továbbra is fennáll, hozz létre hibajelentést.',
+    'drawing' => 'Rajzolás',
+    'withoutTitle' => 'Ennek a világtérképnek nincs címe, ezért alapértelmezés szerint mindig a "Világ térkép"-re kattintáskor használhatod. Ahhoz, hogy új világ térképet hozz létre, ennek adnod kell egy címet.',
   ),
   'pointCalc' => 
   array (
@@ -20,13 +325,105 @@ return array (
   'tableGenerator' => 
   array (
     'title' => 'Táblázat generátor',
+    'playerByAlly' => 'Egy klán összes játékosa',
+    'villageByAlly' => 'Egy klán összes faluja',
+    'villageByPlayer' => 'Egy játékos összes faluja',
+    'villageAndPlayerByAlly' => 'Egy klán összes faluja, beleértve a játékosokat is',
+    'maxSing' => 'A fórum maximum 1000 "[" karaktert fogad el.<br>Ezért a kimenet több táblázatra van felosztva',
   ),
   'accMgrDB' => 
   array (
-    'title' => 'Építési sablon',
+    'title' => 'Építési sablon adatbázis',
+    'building' => 'Épület',
+    'copy' => 'Másolás',
+    'err' => 
+    array (
+      'name_not_found' => 'A sablon túl rövidnek tűnik. Ha ezt hibának tekinti, kérjük, lépjen kapcsolatba a fejlesztőkkel',
+      'paylod_len' => 'A sablon túl rövidnek tűnik. Ha ezt hibának tekinti, kérjük, lépjen kapcsolatba a fejlesztőkkel',
+      'unknown_building' => 'A sablon egy ismeretlen épületet tartalmaz. Kérjük, lépj kapcsolatba a fejlesztőkkel',
+    ),
+    'errors' => 
+    array (
+      'aboveMaxLevel' => 'Túl magas az épület szintje!',
+      'farmLow' => 'A tanya megtelt!',
+      'storageLow' => 'A raktár túl kicsi!',
+    ),
+    'export' => 'Exportálás',
+    'import' => 'Importálás',
+    'export_success' => 'A sablont a vágólapra másoltad',
+    'filter' => 
+    array (
+      'church' => 'templomot tartalmazhat',
+      'statue' => 'szobrot tartalmazhat',
+      'watchtower' => 'őrtornyot tartalmazhat',
+    ),
+    'leaveMessage' => 'A sablon nem lett elmentve. Törli?',
+    'level' => 'Szint',
+    'name' => 'Név',
+    'name_def' => 'Sablon',
+    'points' => 'Pont',
+    'public' => 'Tegye nyilvánossá',
+    'remAddit' => 'távolítsa el a felesleges épületeket',
+    'remChurch' => 'Hagyja figyelmen kívül a templomot',
+    'remFirstChurch' => 'Hagyja figyelmen kívül az első templomot',
+    'remWT' => 'Hagyja figyelmen kívül az őrtornyot',
+    'save_error' => 'A sablont nem sikerült elmenteni. Vannak épületek?',
+    'save_success' => 'A sablon sikeresen mentve',
+    'showLink' => 'Link megtekintése',
+    'showLinkDesc' => 'Ez a link hozzáférést biztosít a sablonhoz.',
+    'table' => 
+    array (
+      'creator' => 'Készítő',
+      'name' => 'Név',
+      'rating' => 'Értékelés',
+      'type' => 'Világ típusa',
+    ),
+    'title_edit' => 'Sablon szerkesztése',
+    'title_show' => 'Sablon megjelenítése',
   ),
   'animHistMap' => 
   array (
     'title' => 'Térkép készítése',
+    'destroyError' => 'Az animált térképet nem sikerült törölni. Ha úgy gondolja, hogy ez hiba, forduljon a csapathoz (a Discord segítségével vagy a probléma jelentésével a webhely alján).',
+    'destroySuccess' => 'Az animált térkép sikeresen törölve',
+    'download' => 
+    array (
+      'gif' => 'Gif letöltése',
+      'mp4' => 'Mp4 letöltése',
+      'zip' => 'Zip letöltése',
+    ),
+    'edit' => 'Címkék szerkesztése',
+    'editLink' => 'Link szerkesztése',
+    'editLinkDesc' => 'A link birtokában mindenki szerkesztheti az animált térképet. Légy óvatos!',
+    'fastSwitch' => 'Saját animált térképek',
+    'legend' => 'Legenda',
+    'links' => 'Linkek a megosztáshoz',
+    'preview' => 'Előnézet',
+    'render' => 
+    array (
+      'finished' => 'Kész',
+      'gifAdd' => 'Gif létrehozása (olvasás: {numImage} - {totalImage})',
+      'gifWrite' => 'Gif létrehozása (írás: {numImage} - {totalImage})',
+      'image' => 'Kép készítése: {numImage} -{totalImage}',
+      'mp4' => 'Mp4 video készítése',
+      'queue' => 'sorban',
+      'zip' => 'Zip létrehozása',
+    ),
+    'renderedDestroyError' => 'A megjelenített animált térképet nem lehetett törölni. Ha úgy gondolod, hogy ez hiba, fordulj a csapathoz (a Discord segítségével vagy a probléma jelentésével a webhely alján).',
+    'renderedDestroySuccess' => 'Renderelt animált térkép sikeresen törölve',
+    'renderNow' => 'Hozd létre azonnal!',
+    'rerun' => 'Készítsd el újra',
+    'settings' => 'Általános beállítások',
+    'showLink' => 'Link megtekintése',
+    'showLinkDesc' => 'Ez a hivatkozás hozzáférést biztosít az animált térképhez, de szerkeszteni nem tudod.',
+    'withoutTitle' => 'Ennek az animált világtérképnek nincs címe, ezért alapértelmezés szerint mindig a "Térkép készítése" gombra kattintva használhatod. Ahhoz, hogy új animált világtérképet készíthess vele, adnod kell neki egy címet.',
+  ),
+  'attackplaner' => 
+  array (
+    'notes' => 'Jegyzetek',
+  ),
+  'greatSiegeCalc' => 
+  array (
+    'title' => 'Nagy csata kalkulátor',
   ),
 );
